00d899657c9dfec5cfab81deba304164d9e7fc71
Caleb Buxton <me@cpb.ca>
2014-02-11 22:44:07 -0800

    Adding old collection matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'opennorth/represent'
 require 'pry'
 
+require 'rspec/collection_matchers'
+
 require 'simplecov'
 
 Dir[File.join(File.dirname(__FILE__),"support","**","*.rb")].each { |f| require f }
