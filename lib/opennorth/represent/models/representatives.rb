2e0eb1c4dc939670c6589c8584fa56fe7fbaf74c
Caleb Buxton <me@cpb.ca>
2014-02-12 00:19:35 -0800

    Implemented simple #where for representatives

--- lib/opennorth/represent/models/representatives.rb
+++ lib/opennorth/represent/models/representatives.rb
@@ -5,6 +5,13 @@ module Opennorth
     class Representatives < Fog::Collection
       model Opennorth::Represent::Representative
 
+      def where(attribute_value_map)
+        select do |rep|
+          attribute_value_map.all? do |(attribute, value)|
+            rep.send(attribute) == value
+          end
+        end
+      end
     end
   end
 end
--- spec/opennorth/represent/models/representatives_spec.rb
+++ spec/opennorth/represent/models/representatives_spec.rb
@@ -4,6 +4,23 @@ require 'opennorth/represent/models/representatives'
 describe Opennorth::Represent::Representatives, type: :collection do
   let(:service) { Opennorth::Represent.new({}) }
 
-  it "#all"
-  it "#find"
+  let(:sample_set) { service.get_postal_code("V6H2V4").body["representatives_centroid"] }
+
+
+  context "loaded with sample data" do
+
+    let(:loaded_collection) { described_collection.load(sample_set) }
+
+    describe "#where" do
+      subject { loaded_collection.where(elected_office: "MP") }
+
+      it "should return a collection" do
+        expect(subject).to have(1).item
+      end
+
+      it "should contain only matches" do
+        expect(subject).to be_all {|rep| rep.elected_office == "MP"}
+      end
+    end
+  end
 end
